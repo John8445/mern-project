be2e2a70c470ee1da1a0527e20c94057af40be81
John8445 <halljohn3366@gmail.com>
2023-06-04 13:09:42 -0400

    Update index.js for readability and add new files

--- server/index.js
+++ server/index.js
@@ -18,10 +18,12 @@ import User from "./models/User.js";
 import Post from "./models/Post.js";
 import { users, posts } from "./data/index.js";
 
-/* CONFIGURATIONS */
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 dotenv.config();
+
+const PORT = process.env.PORT || 6001;
+const MONGO_URL = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.DB_NAME}.ij9jsqc.mongodb.net/`
+
+
 const app = express();
 app.use(express.json());
 app.use(helmet());
@@ -30,9 +32,7 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
-/* FILE STORAGE */
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "public/assets");
@@ -41,29 +41,34 @@ const storage = multer.diskStorage({
     cb(null, file.originalname);
   },
 });
+
 const upload = multer({ storage });
 
-/* ROUTES WITH FILES */
 app.post("/auth/register", upload.single("picture"), register);
 app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
-/* ROUTES */
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
 
 /* MONGOOSE SETUP */
-const PORT = process.env.PORT || 6001;
-mongoose
-  .connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+const connectToDatabase = async () => {
+  try {
+    await mongoose.connect(MONGO_URL, {
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true,
+    });
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error(`Error connecting to MongoDB: ${error.message}`);
+  }
+};
 
-    /* ADD DATA ONE TIME */
-    // User.insertMany(users);
-    // Post.insertMany(posts);
-  })
-  .catch((error) => console.log(`${error} did not connect`));
+const startServer = async () => {
+  await connectToDatabase();
+  app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  // User.insertMany(users);
+  // Post.insertMany(posts);
+};
+
+startServer();
